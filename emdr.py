6f197908df94198a34f359e6f753fa5134e58992
Vijay_Mike <79729617+CourageTrain@users.noreply.github.com>
2024-07-11 15:07:21 -0700

    Update emdr.py
    Measuring the latency of the mediapipe calls -
    Sample video - of problem statement

--- emdr.py
+++ emdr.py
@@ -1,34 +1,38 @@
 
+import cProfile
 
 import cv2
 import mediapipe as mp
 import matplotlib.pyplot as plt
 import numpy as np
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-mp_draw = mp.solutions.drawing_utils
 
-thumb_positions = []
 
-cap = cv2.VideoCapture(0)
+def main():
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_draw = mp.solutions.drawing_utils
+    thumb_positions = []
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                thumbtip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                thumbtip_x, thumbtip_y = int(thumbtip.x * frame.shape[1]), int(thumbtip.y * frame.shape[0])
+                thumb_x, thumb_y = int(thumbtip.x*frame.shape[1]), int(thumbtip.y * frame.shape[0])
+                thumb_positions.append((thumb_x, thumb_y))
+        cv2.imshow("Thumb Movement", frame)
 
-while True:
-    success, frame = cap.read()
-    if not success:
-        break
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(frame_rgb)
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            thumbtip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-            thumbtip_x, thumbtip_y = int(thumbtip.x * frame.shape[1]), int(thumbtip.y * frame.shape[0])
-            thumb_x, thumb_y = int(thumbtip.x*frame.shape[1]), int(thumbtip.y * frame.shape[0])
-            thumb_positions.append((thumb_x, thumb_y))
-    cv2.imshow("Thumb Movement", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            cap.release()
+            cv2.destroyAllWindows()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-        cap.release()
-        cv2.destroyAllWindows()
+if __name__ == "__main__":
+    cProfile.run('main()', 'profile.prof')
